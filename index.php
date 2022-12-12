9c1f3230ae17c97d57fe7ac336fd14493ab57e0e
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-12-12 17:31:12 +0100

    creazione classe product + method + costruttore

--- index.php
+++ index.php
@@ -7,3 +7,36 @@
 // Stampiamo delle card contenenti i dettagli dei prodotti,
 // come immagine, titolo, prezzo, icona della categoria ed il tipo di articolo che si sta visualizzando 
 // (prodotto, cibo, gioco, cuccia).
+
+class Product
+{
+    public $name;
+    public $type;
+    public $description;
+    public $price;
+    public $category;
+
+    public function __construct(string $name, string $description, string $type, int $price, string $category)
+    {
+        $this->name = $name;
+        $this->type = $type;
+        $this->description = $description;
+        $this->price = $price;
+        $this->set_category($category);
+    }
+
+    public function set_category($category)
+    {
+     if ($category == 'Cane') {
+        $this->category = <i class="fa-solid fa-dog"></i>;
+        else if ($category == 'Gatto') {
+        $this->category = <i class="fa-solid fa-cat"></i>;
+        }
+     }
+    }
+    public function get_category( $category)
+    {
+    return $this->category;
+    }
+}
+?>
